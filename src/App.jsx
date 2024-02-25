4f24de5a4d0d18ce9be93e719348dc7d4dbea731
SaulEspinoza7 <ciberelmas4@gmail.com>
2024-02-25 13:47:47 -0500

    25

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   const [details, setDetails] = useState([])
   const [placeType, setPlaceType] = useState("Restaurants");
-  const [globalUrl, setGlobalUrl] = useState("https://7cba0a41a17a.ngrok.app/")
+  const [globalUrl, setGlobalUrl] = useState("https://7659ce6d62a4.ngrok.app/")
 
   return (
     <BrowserRouter>
